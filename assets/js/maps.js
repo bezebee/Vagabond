8b0dc652865e43570723baafc458e67ad0bb98ce
bezebee <kas.bogucka@gmail.com>
2020-04-24 10:21:40 +0000

    Updated maps.js to include multiple maps on one page.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,15 +1,2 @@
-function initMap() {
-  var myLatLng = {lat: 60.1282, lng: 18.6435};
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: myLatLng
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Sweden'
-  });
-}
 
