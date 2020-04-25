09fe5e70e9e95565c5eb3305dfb36157b23c19a3
bezebee <kas.bogucka@gmail.com>
2020-04-25 08:04:40 +0000

    Tool tip added on hover to lead to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -43,7 +43,7 @@ function initMap() {
   for (i = 0; i < markers.length; i++) {
     lat = markers[i].Coord1;
     lng = markers[i].Coord2;
-    name = markers[i].client_address;
+    name = markers[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -65,7 +65,7 @@ var markers2 = [
   {
     Coord1: 4.6482837,
     Coord2: -74.2478945,
-    client_address: "colombia",
+    place_name: "colombia",
   },
 ];
 
@@ -87,7 +87,7 @@ function initMap2() {
   for (i = 0; i < markers2.length; i++) {
     lat = markers2[i].Coord1;
     lng = markers2[i].Coord2;
-    name = markers2[i].client_address;
+    name = markers2[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -110,7 +110,7 @@ var markers3 = [
   {
     Coord1: 51.1657,
     Coord2: 10.4515,
-    client_address: "Germany",
+    place_name: "Germany",
   },
 ];
 
@@ -132,7 +132,7 @@ function initMap3() {
   for (i = 0; i < markers3.length; i++) {
     lat = markers3[i].Coord1;
     lng = markers3[i].Coord2;
-    name = markers3[i].client_address;
+    name = markers3[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -155,7 +155,7 @@ var markers4 = [
   {
     Coord1: 48.8589507,
     Coord2: 2.2770204,
-    client_address: "France",
+    place_name: "France",
   },
 ];
 
@@ -177,7 +177,7 @@ function initMap4() {
   for (i = 0; i < markers4.length; i++) {
     lat = markers4[i].Coord1;
     lng = markers4[i].Coord2;
-    name = markers4[i].client_address;
+    name = markers4[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -201,7 +201,7 @@ var markers5 = [
   {
     Coord1: 6.690251,
     Coord2: -1.6861462,
-    client_address: "Ghana",
+    place_name: "Ghana",
   },
 ];
 
@@ -223,7 +223,7 @@ function initMap5() {
   for (i = 0; i < markers5.length; i++) {
     lat = markers5[i].Coord1;
     lng = markers5[i].Coord2;
-    name = markers5[i].client_address;
+    name = markers5[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -248,7 +248,7 @@ var markers6 = [
   {
     Coord1: 64.1335484,
     Coord2: -21.9224813,
-    client_address: "Iceland",
+    place_name: "Iceland",
   },
 ];
 
@@ -270,7 +270,7 @@ function initMap6() {
   for (i = 0; i < markers6.length; i++) {
     lat = markers6[i].Coord1;
     lng = markers6[i].Coord2;
-    name = markers6[i].client_address;
+    name = markers6[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -295,7 +295,7 @@ var markers7 = [
   {
     Coord1: 53.3244431,
     Coord2: -6.3857857,
-    client_address: "Ireland",
+    place_name: "Ireland",
   },
 ];
 
@@ -317,7 +317,7 @@ function initMap7() {
   for (i = 0; i < markers7.length; i++) {
     lat = markers7[i].Coord1;
     lng = markers7[i].Coord2;
-    name = markers7[i].client_address;
+    name = markers7[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -342,7 +342,7 @@ var markers8 = [
   {
     Coord1: 35.6684415,
     Coord2: 139.6007843,
-    client_address: "Japan",
+    place_name: "Japan",
   },
 ];
 
@@ -364,7 +364,7 @@ function initMap8() {
   for (i = 0; i < markers8.length; i++) {
     lat = markers8[i].Coord1;
     lng = markers8[i].Coord2;
-    name = markers8[i].client_address;
+    name = markers8[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -389,7 +389,7 @@ var markers9 = [
   {
     Coord1: -5.7379831,
     Coord2: -64.1876637,
-    client_address: "Latin America",
+    place_name: "Latin America",
   },
 ];
 
@@ -414,7 +414,7 @@ function initMap9() {
   for (i = 0; i < markers9.length; i++) {
     lat = markers9[i].Coord1;
     lng = markers9[i].Coord2;
-    name = markers9[i].client_address;
+    name = markers9[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -439,7 +439,7 @@ var markers10 = [
   {
     Coord1: 40.6971494,
     Coord2: -74.2598655,
-    client_address: "New York",
+    place_name: "New York",
   },
 ];
 
@@ -461,7 +461,7 @@ function initMap10() {
   for (i = 0; i < markers10.length; i++) {
     lat = markers10[i].Coord1;
     lng = markers10[i].Coord2;
-    name = markers10[i].client_address;
+    name = markers10[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -486,7 +486,7 @@ var markers11 = [
   {
     Coord1: 55.5815245,
     Coord2: 36.8251381,
-    client_address: "Russia",
+    place_name: "Russia",
   },
 ];
 
@@ -508,7 +508,7 @@ function initMap11() {
   for (i = 0; i < markers11.length; i++) {
     lat = markers11[i].Coord1;
     lng = markers11[i].Coord2;
-    name = markers11[i].client_address;
+    name = markers11[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -533,7 +533,7 @@ var markers12 = [
   {
     Coord1: 39.9390731,
     Coord2: 116.1172782,
-    client_address: "China",
+    place_name: "China",
   },
 ];
 
@@ -555,7 +555,7 @@ function initMap12() {
   for (i = 0; i < markers12.length; i++) {
     lat = markers12[i].Coord1;
     lng = markers12[i].Coord2;
-    name = markers12[i].client_address;
+    name = markers12[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -580,7 +580,7 @@ var markers13 = [
   {
     Coord1: 59.326242,
     Coord2: 17.8419717,
-    client_address: "Sweden",
+    place_name: "Sweden",
   },
 ];
 
@@ -602,7 +602,7 @@ function initMap13() {
   for (i = 0; i < markers13.length; i++) {
     lat = markers13[i].Coord1;
     lng = markers13[i].Coord2;
-    name = markers13[i].client_address;
+    name = markers13[i].place_name;
 
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, lng),
@@ -620,3 +620,7 @@ function initMap13() {
     );
   }
 }
+
+// hover over placemark effect
+
+
