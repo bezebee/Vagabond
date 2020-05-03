dec4808f5362ab2b4020d334277148499839e14d
Kasia Bee <kas.bogucka@gmail.com>
2020-05-03 17:21:32 +0000

    Newsletter subscription form added to five main pages and About section. Tested all six by sending email

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,13 +1,11 @@
-function sendMail(contactForm) {
-  emailjs
-    .send("gmail", "contact_form", {
-      from_name: contactForm.name.value,
-      from_email: contactForm.email_address.value,
-      message: contactForm.message.value,
+ function sendMail(Newsletter) {
+  emailjs.send("gmail", "subscription_success", {
+      "email": Newsletter.email.value
     })
     .then(
       function (response) {
         console.log("SUCCESS", response);
+        alert(subscribed)
       },
       function (error) {
         console.log("FAILED", error);
@@ -15,6 +13,3 @@ function sendMail(contactForm) {
     );
   return false; // To block from loading a new page
 }
-
-
-  
