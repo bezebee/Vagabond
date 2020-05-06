0bbd4884c7df96cfd2321de597cd495524856791
Kasia Bee <kas.bogucka@gmail.com>
2020-05-06 13:01:56 +0000

    Option of adding and removing currency added to the converter

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -1,268 +1,310 @@
-      //Gobal variables
-    
-    const addCurrencyBtn = document.querySelector(".add-currency-btn");
-    const addCurrencyList = document.querySelector(".add-currency-list");
-    const currenciesList = document.querySelector(".currencies"); 
+//Gobal variables
 
-    const initiallyDisplayedCurrencies = ["AUD", "EUR", "GBP", "JPY", "RUB"];
-    let baseCurrency;
-    let baseCurrencyAmount;
+const addCurrencyBtn = document.querySelector(".add-currency-btn");
+const addCurrencyList = document.querySelector(".add-currency-list");
+const currenciesList = document.querySelector(".currencies");
 
-    const currencies = [
-    {
+const initiallyDisplayedCurrencies = ["AUD", "EUR", "GBP", "JPY", "RUB"];
+let baseCurrency;
+let baseCurrencyAmount;
+
+const currencies = [
+  {
     name: "US Dollar",
     abbreviation: "USD",
     symbol: "\u0024",
     flagURL: "http://www.geonames.org/flags/l/us.gif",
-    rate: 1.1345
+    rate: 1.1345,
   },
   {
     name: "Euro",
     abbreviation: "EUR",
     symbol: "\u20AC",
-    flagURL: "https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg",
-    rate: 13.90
+    flagURL:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg",
+    rate: 13.9,
   },
   {
     name: "Japanese Yen",
     abbreviation: "JPY",
     symbol: "\u00A5",
     flagURL: "http://www.geonames.org/flags/l/jp.gif",
-    rate: 8.9073 
+    rate: 8.9073,
   },
   {
     name: "British Pound",
     abbreviation: "GBP",
     symbol: "\u00A3",
     flagURL: "http://www.geonames.org/flags/l/uk.gif",
-    rate: 1.2340
+    rate: 1.234,
   },
   {
     name: "Australian Dollar",
     abbreviation: "AUD",
     symbol: "\u0024",
-    flagURL: "http://www.geonames.org/flags/l/au.gif"
+    flagURL: "http://www.geonames.org/flags/l/au.gif",
   },
   {
     name: "Canadian Dollar",
     abbreviation: "CAD",
     symbol: "\u0024",
-    flagURL: "http://www.geonames.org/flags/l/ca.gif"
+    flagURL: "http://www.geonames.org/flags/l/ca.gif",
   },
   {
     name: "Swiss Franc",
     abbreviation: "CHF",
     symbol: "\u0043\u0048\u0046",
-    flagURL: "http://www.geonames.org/flags/l/ch.gif"
+    flagURL: "http://www.geonames.org/flags/l/ch.gif",
   },
   {
     name: "Chinese Yuan Renminbi",
     abbreviation: "CNY",
     symbol: "\u00A5",
-    flagURL: "http://www.geonames.org/flags/l/cn.gif"
+    flagURL: "http://www.geonames.org/flags/l/cn.gif",
   },
   {
     name: "Swedish Krona",
     abbreviation: "SEK",
     symbol: "\u006B\u0072",
-    flagURL: "http://www.geonames.org/flags/l/se.gif"
+    flagURL: "http://www.geonames.org/flags/l/se.gif",
   },
   {
     name: "New Zealand Dollar",
     abbreviation: "NZD",
     symbol: "\u0024",
-    flagURL: "http://www.geonames.org/flags/l/nz.gif"
+    flagURL: "http://www.geonames.org/flags/l/nz.gif",
   },
   {
     name: "Mexican Peso",
     abbreviation: "MXN",
     symbol: "\u0024",
-    flagURL: "http://www.geonames.org/flags/l/mx.gif"
+    flagURL: "http://www.geonames.org/flags/l/mx.gif",
   },
   {
     name: "Singapore Dollar",
     abbreviation: "SGD",
     symbol: "\u0024",
-    flagURL: "http://www.geonames.org/flags/l/sg.gif"
+    flagURL: "http://www.geonames.org/flags/l/sg.gif",
   },
   {
     name: "Hong Kong Dollar",
     abbreviation: "HKD",
     symbol: "\u0024",
-    flagURL: "http://www.geonames.org/flags/l/hk.gif"
+    flagURL: "http://www.geonames.org/flags/l/hk.gif",
   },
   {
     name: "Norwegian Krone",
     abbreviation: "NOK",
     symbol: "\u006B\u0072",
-    flagURL: "http://www.geonames.org/flags/l/no.gif"
+    flagURL: "http://www.geonames.org/flags/l/no.gif",
   },
   {
     name: "South Korean Won",
     abbreviation: "KRW",
     symbol: "\u20A9",
-    flagURL: "http://www.geonames.org/flags/l/kr.gif"
+    flagURL: "http://www.geonames.org/flags/l/kr.gif",
   },
   {
     name: "Turkish Lira",
     abbreviation: "TRY",
     symbol: "\u20BA",
-    flagURL: "http://www.geonames.org/flags/l/tr.gif"
+    flagURL: "http://www.geonames.org/flags/l/tr.gif",
   },
   {
     name: "Russian Ruble",
     abbreviation: "RUB",
     symbol: "\u20BD",
-    flagURL: "http://www.geonames.org/flags/l/ru.gif"
+    flagURL: "http://www.geonames.org/flags/l/ru.gif",
   },
   {
     name: "Indian Rupee",
     abbreviation: "INR",
     symbol: "\u20B9",
-    flagURL: "http://www.geonames.org/flags/l/in.gif"
+    flagURL: "http://www.geonames.org/flags/l/in.gif",
   },
   {
     name: "Brazilian Real",
     abbreviation: "BRL",
     symbol: "\u0052\u0024",
-    flagURL: "http://www.geonames.org/flags/l/br.gif"
+    flagURL: "http://www.geonames.org/flags/l/br.gif",
   },
   {
     name: "South African Rand",
     abbreviation: "ZAR",
     symbol: "\u0052",
-    flagURL: "http://www.geonames.org/flags/l/za.gif"
+    flagURL: "http://www.geonames.org/flags/l/za.gif",
   },
   {
     name: "Philippine Peso",
     abbreviation: "PHP",
     symbol: "\u20B1",
-    flagURL: "http://www.geonames.org/flags/l/ph.gif"
+    flagURL: "http://www.geonames.org/flags/l/ph.gif",
   },
   {
     name: "Czech Koruna",
     abbreviation: "CZK",
     symbol: "\u004B\u010D",
-    flagURL: "http://www.geonames.org/flags/l/cz.gif"
+    flagURL: "http://www.geonames.org/flags/l/cz.gif",
   },
   {
     name: "Indonesian Rupiah",
     abbreviation: "IDR",
     symbol: "\u0052\u0070",
-    flagURL: "http://www.geonames.org/flags/l/id.gif"
+    flagURL: "http://www.geonames.org/flags/l/id.gif",
   },
   {
     name: "Malaysian Ringgit",
     abbreviation: "MYR",
     symbol: "\u0052\u004D",
-    flagURL: "http://www.geonames.org/flags/l/my.gif"
+    flagURL: "http://www.geonames.org/flags/l/my.gif",
   },
   {
     name: "Hungarian Forint",
     abbreviation: "HUF",
     symbol: "\u0046\u0074",
-    flagURL: "http://www.geonames.org/flags/l/hu.gif"
+    flagURL: "http://www.geonames.org/flags/l/hu.gif",
   },
   {
     name: "Icelandic Krona",
     abbreviation: "ISK",
     symbol: "\u006B\u0072",
-    flagURL: "http://www.geonames.org/flags/l/is.gif"
+    flagURL: "http://www.geonames.org/flags/l/is.gif",
   },
   {
     name: "Croatian Kuna",
     abbreviation: "HRK",
     symbol: "\u006B\u006E",
-    flagURL: "http://www.geonames.org/flags/l/hr.gif"
+    flagURL: "http://www.geonames.org/flags/l/hr.gif",
   },
   {
     name: "Bulgarian Lev",
     abbreviation: "BGN",
     symbol: "\u043B\u0432",
-    flagURL: "http://www.geonames.org/flags/l/bg.gif"
+    flagURL: "http://www.geonames.org/flags/l/bg.gif",
   },
   {
     name: "Romanian Leu",
     abbreviation: "RON",
     symbol: "\u006C\u0065\u0069",
-    flagURL: "http://www.geonames.org/flags/l/ro.gif"
+    flagURL: "http://www.geonames.org/flags/l/ro.gif",
   },
   {
     name: "Danish Krone",
     abbreviation: "DKK",
     symbol: "\u006B\u0072",
-    flagURL: "http://www.geonames.org/flags/l/dk.gif"
+    flagURL: "http://www.geonames.org/flags/l/dk.gif",
   },
   {
     name: "Thai Baht",
     abbreviation: "THB",
     symbol: "\u0E3F",
-    flagURL: "http://www.geonames.org/flags/l/th.gif"
+    flagURL: "http://www.geonames.org/flags/l/th.gif",
   },
   {
     name: "Polish Zloty",
     abbreviation: "PLN",
     symbol: "\u007A\u0142",
-    flagURL: "http://www.geonames.org/flags/l/pl.gif"
+    flagURL: "http://www.geonames.org/flags/l/pl.gif",
   },
   {
     name: "Israeli Shekel",
     abbreviation: "ILS",
     symbol: "\u20AA",
-    flagURL: "http://www.geonames.org/flags/l/il.gif"
+    flagURL: "http://www.geonames.org/flags/l/il.gif",
+  },
+];
+
+//Event Listeners
+
+addCurrencyBtn.addEventListener("click", addCurrencyBtnClick);
+
+function addCurrencyBtnClick(event) {
+  addCurrencyBtn.classList.toggle("open");
+}
+//add currency to the list
+addCurrencyList.addEventListener("click", addCurrencyListClick);
+
+function addCurrencyListClick(event) {
+  const clickedListItem = event.target.closest("li");
+  if (!clickedListItem.classList.contains("disabled")) {
+    const newCurrency = currencies.find(
+      (c) => c.abbreviation === clickedListItem.getAttribute("data-currency")
+    );
+    if (newCurrency) newCurrenciesListItem(newCurrency);
   }
-    ];
+}
+//coding the span button to remove a currency from the main list
+currenciesList.addEventListener("click", currenciesListClick);
 
-    //Event Listeners
+function currenciesListClick(event) {
+  if (event.target.classList.contains("close")) {
+    const parentNode = event.target.parentNode;
+    parentNode.remove();
+  }
+}
 
-    addCurrencyBtn.addEventListener("click", addCurrencyBtnClick);
+//Auxiliary Functions
 
-    function addCurrencyBtnClick(event){
-        addCurrencyBtn.classList.toggle("open")
-    }
-
-    //Auxiliary Functions
-
-    function populateAddCurrencyList(){
-        for(let i=0; i<currencies.length; i++) {
-         addCurrencyList.insertAdjacentHTML("beforeend",
-         `<li class="currency list-group-item" data-currency=${currencies[i].abbreviation}>
+function populateAddCurrencyList() {
+  for (let i = 0; i < currencies.length; i++) {
+    addCurrencyList.insertAdjacentHTML(
+      "beforeend",
+      `<li class="currency list-group-item" data-currency=${currencies[i].abbreviation}>
         <img src=${currencies[i].flagURL} class="flag"/><span>${currencies[i].abbreviation}<br>${currencies[i].name}</span>
-              </li>`);
-        }
-    }
+              </li>`
+    );
+  }
+}
 
-    function populateCurrenciesList(){
-        for (let i=0; i<initiallyDisplayedCurrencies.length; i++){
-           const currency = currencies.find(c => c.abbreviation === initiallyDisplayedCurrencies[i]);
-           if(currency) newCurrenciesListItem(currency);
-        }
-    }
+function populateCurrenciesList() {
+  for (let i = 0; i < initiallyDisplayedCurrencies.length; i++) {
+    const currency = currencies.find(
+      (c) => c.abbreviation === initiallyDisplayedCurrencies[i]
+    );
+    if (currency) newCurrenciesListItem(currency);
+  }
+}
 
-    function newCurrenciesListItem(currency){
-//checking if list is empty
-if(currenciesList.childElementCount===0){
+function newCurrenciesListItem(currency) {
+  //checking if list is empty
+  if (currenciesList.childElementCount === 0) {
     baseCurrency = currency.abbreviation;
     baseCurrencyAmount = 0;
-}
-//disabling responding currency from currency list
-addCurrencyList.querySelector(`[data-currency=${currency.abbreviation}]`).classList.add("disabled");
-const baseCurrencyRate = currencies.find(c => c.abbreviation===baseCurrency).rate;
-const exchangeRate = currency.abbreviation === baseCurrency ? 1 : (currency.rate/baseCurrencyRate).toFixed(4);
-const inputValue = baseCurrencyAmount ? (baseCurrencyAmount*exchangeRate).toFixed(4) : "";
+  }
+  //disabling responding currency from currency list
+  addCurrencyList
+    .querySelector(`[data-currency=${currency.abbreviation}]`)
+    .classList.add("disabled");
+  const baseCurrencyRate = currencies.find(
+    (c) => c.abbreviation === baseCurrency
+  ).rate;
+  const exchangeRate =
+    currency.abbreviation === baseCurrency
+      ? 1
+      : (currency.rate / baseCurrencyRate).toFixed(4);
+  const inputValue = baseCurrencyAmount
+    ? (baseCurrencyAmount * exchangeRate).toFixed(4)
+    : "";
 
-currenciesList.insertAdjacentHTML(
-    "beforeend", 
-    `<li class="currency ${currency.abbreviation===baseCurrency ? "base-currency" : ""}" id=${currency.abbreviation}>
+  currenciesList.insertAdjacentHTML(
+    "beforeend",
+    `<li class="currency ${
+      currency.abbreviation === baseCurrency ? "base-currency" : ""
+    }" id=${currency.abbreviation}>
+    <img src=${
+      currency.flagURL
+    } class="flag"><input class="curr-place" placeholder="0.0000" value=${inputValue} >
     <div class="info">
-    <p class="input flag"><img src=${currency.flagURL} class="flag"><input class="curr-place" placeholder="0.0000" value=${inputValue} ></p>
+    <p class="input flag"></p>
     <p class="currency-name">${currency.abbreviation} - ${currency.name}</p><br>
-    <p class="base-currency-rate">1 ${baseCurrency} = ${exchangeRate} ${currency.abbreviation}</p>
+    <p class="base-currency-rate">1 ${baseCurrency} = ${exchangeRate} ${
+      currency.abbreviation
+    }</p>
     </div>
     <span class="close">&times;</span>
     </li>`
-);
-    }
-    populateAddCurrencyList();
+  );
+}
+populateAddCurrencyList();
 
-    populateCurrenciesList();
+populateCurrenciesList();
