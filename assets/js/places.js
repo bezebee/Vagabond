b0906d3cf46524da9936d22ee2b0cabc837ce52a
Kasia Bee <kas.bogucka@gmail.com>
2020-05-10 20:49:14 +0000

    Working on implementing the search bar

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,21 +1,38 @@
 
-
 $.getJSON('https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
   console.log(data);
   
-  
-  var imageList = data.results;
+
+  let imageList = data.results;
+
+  $.each(data, function(index,value){
+      console.log(value);
+  });
   
   $.each(imageList, function(i, val) {
     
-    var image = val;
-    var imageURL = val.urls.regular;
-    var imageWidth = val.width;
-    var imageHeight = val.height;
+    let image = val;
+    let imageURL = val.urls.regular;
+    let imageWidth = val.width;
+    let imageHeight = val.height;
+   
     
     if (imageWidth > imageHeight) {
-      $('.row.api').append('<div class="api-gallery"><img src="'+ imageURL +'"></div>');
+      $('header.output').append('<div class="div.image-api"><img src="'+ imageURL +'"></div>');
     }   
     
   });  
 });
+
+
+const searchBar = document.getElementById('searchBar');
+let value = [];
+
+searchBar.addEventListener('keyup',(e) => {
+const searchString = e.target.value;
+const filteredImages = value.filter((image) =>{
+    return(
+    image.user.includes(searchString)|| image.tags.includes(searchString)
+    );
+});
+console.log(filteredImages)});
