74a555569ccc1bf5b744b80c2058a4ad5503a348
Kasia Bee <kas.bogucka@gmail.com>
2020-05-10 12:50:49 +0000

    Initial containers set, work in progress

--- assets/js/places.js
+++ assets/js/places.js
@@ -2,6 +2,11 @@ $.getJSON('https://api.unsplash.com/photos/?client_id=kZ9sSryGBssi-2T5OUPjVLye-D
     console.log(data);
     $.each(data, function(index, value){
         console.log(value);
+
+        var name = value.user.name;
+        var bio = value.user.bio;
+        var imageURL = value.urls.regular;
+        
     });
 });
 
