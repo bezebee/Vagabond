2bdf355c7e03fb615ae058becd5d75d2354b3dd4
Kasia Bee <kas.bogucka@gmail.com>
2020-05-10 13:26:30 +0000

    Created a loop of images by User and Bio

--- assets/js/places.js
+++ assets/js/places.js
@@ -6,6 +6,12 @@ $.getJSON('https://api.unsplash.com/photos/?client_id=kZ9sSryGBssi-2T5OUPjVLye-D
         var name = value.user.name;
         var bio = value.user.bio;
         var imageURL = value.urls.regular;
+
+        // $('.name').text(name);
+        // $('.bio').text(bio);
+        // $('.image img').attr('src',imageURL);
+
+        $('.output').append('<h1 class="name">' + name + '</h1><h2 class="bio">' + bio + '</h2><div class="image"><img src="' + imageURL +'"/></div>')
         
     });
 });
