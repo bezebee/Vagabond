52ab9a845f965c29fac0cad0407ca8f3709e9c0a
Kasia Bee <kas.bogucka@gmail.com>
2020-05-10 20:49:48 +0000

    Project saved and work is suspended until the next session

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,38 +1,38 @@
 
-$.getJSON('https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
-  console.log(data);
+// $.getJSON('https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
+//   console.log(data);
   
 
-  let imageList = data.results;
+//   let imageList = data.results;
 
-  $.each(data, function(index,value){
-      console.log(value);
-  });
+//   $.each(data, function(index,value){
+//       console.log(value);
+//   });
   
-  $.each(imageList, function(i, val) {
+//   $.each(imageList, function(i, val) {
     
-    let image = val;
-    let imageURL = val.urls.regular;
-    let imageWidth = val.width;
-    let imageHeight = val.height;
+//     let image = val;
+//     let imageURL = val.urls.regular;
+//     let imageWidth = val.width;
+//     let imageHeight = val.height;
    
     
-    if (imageWidth > imageHeight) {
-      $('header.output').append('<div class="div.image-api"><img src="'+ imageURL +'"></div>');
-    }   
+//     if (imageWidth > imageHeight) {
+//       $('header.output').append('<div class="div.image-api"><img src="'+ imageURL +'"></div>');
+//     }   
     
-  });  
-});
+//   });  
+// });
 
 
-const searchBar = document.getElementById('searchBar');
-let value = [];
+// const searchBar = document.getElementById('searchBar');
+// let value = [];
 
-searchBar.addEventListener('keyup',(e) => {
-const searchString = e.target.value;
-const filteredImages = value.filter((image) =>{
-    return(
-    image.user.includes(searchString)|| image.tags.includes(searchString)
-    );
-});
-console.log(filteredImages)});
+// searchBar.addEventListener('keyup',(e) => {
+// const searchString = e.target.value;
+// const filteredImages = value.filter((image) =>{
+//     return(
+//     image.user.includes(searchString)|| image.tags.includes(searchString)
+//     );
+// });
+// console.log(filteredImages)});
