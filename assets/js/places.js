cb38123c0a2cce543e0277ca2332d9649657fb3f
Kasia Bee <kas.bogucka@gmail.com>
2020-05-11 21:58:43 +0000

    Working on images API, it is proving difficult, further research is needed

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,5 +1,35 @@
+// var url;
 
+// var button = document.getElementById('btn1');
+// button.addEventListener('click',startSearch);
 
+// function startSearch(event){
+//     var userInputValue = document.getElementById('searchBar').value;
+//     var urlBase = 'https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0/search/'
+//     if (userInputValue === null || userInputValue ==='') return;
+//     var searchUrl = urlBase + userInputValue
+//     performSearch(searchUrl);
+// }
+
+// function performSearch(searchUrl){
+//     var requestData = new XMLHttpRequest();
+//     requestData.addEventListener('load', handleResults);
+//     requestData.open('GET',searchUrl);
+//     requestData.send();
+// }
+
+// function handleResults(){
+//     var responseJSON = JSON.parse(this.response);
+
+//     if(responseJSON.error) console.log("Image not found");
+//     else {
+//         var html = '';
+//         responseJSON.results.forEach(function(result){
+//             html+='<div class="div.image-api"><img src="'+ result.image +'"></div>'
+//         })
+//         document.getElementById(".image-api");
+//     }
+// }
 
 $.getJSON('https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
   console.log(data);
