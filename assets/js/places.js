a6441ac1278b2577533420242d113cd27a9cec73
Kasia Bee <kas.bogucka@gmail.com>
2020-05-13 23:23:09 +0000

    Work on image search in progress

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,70 +1,57 @@
 
 
-$.getJSON('https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
-  console.log(data);
-  let imageList = data.results;
-  
-  $.each(data, function(i,value){
-      console.log(i, value);
+
+$.getJSON('https://api.unsplash.com/search/photos?query=europe&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
+console.log(data);
+
+var imageList = data.results;
+
+$.each(imageList, function(i, val){
+
+    var image = data.results;
+
+    var imageURL = val.urls.regular;
+    var imageWidth = val.width;
+    var imageHeight = val.height;
+
+    if(imageWidth>imageHeight){
+        $('.output').append('<div class="image"><img src="'+ imageURL +'"></div>');
+        
+
+};
   });
-  
-  $.each(imageList, function(i, val) { 
-    let imageURL = val.urls.regular;
-    return  $('ul.output').append('<img src="'+ imageURL +'">') 
-  });  
 
-const searchVal = document.getElementById('searchBar').value;
-$.each(imageList, function(i, val) { 
-    let imageURL = val.urls.regular;
-    return  $('section.output').append('<img src="'+ imageURL +'">') 
-  });
+  })
 
 
 
- 
+
+
+  $("button#asia").click(function(){
+
+$.getJSON('https://api.unsplash.com/search/photos?query=asia&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
+console.log(data);
+
+var imageList = data.results;
+
+$.each(imageList, function(i, val){
+
+    var image = data.results[Math.floor(Math.random())]
+
+    var imageURL = val.urls.regular;
+    var imageWidth = val.width;
+    var imageHeight = val.height;
+
+    if(imageWidth>imageHeight){
+        $('.output').append('<div class="image"><img src="'+ imageURL +'"></div>');
+    }else{$('.output').append('<div class="image"><img src="'+ imageURL +'"></div>')}
 });
+  });
 
-var button = document.getElementById('btn');
-// when user clicks on button, we want to call function start search
-button.addEventListener('click', startSearch);
+  })
 
-function startSearch(event) {
 
-  var userInputValue = document.getElementById('searchBar').value;
- 
-  var urlBase = 'https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0'
- 
-  if (userInputValue === null || userInputValue === '') return;
- 
-  var searchUrl = urlBase + userInputValue;
-  
-  performSearch(searchUrl);
-}
 
-function performSearch(searchUrl) {
-  
-  var requestData = new XMLHttpRequest();
-  
-  requestData.addEventListener('load', handleResults);
-  requestData.open('GET', searchUrl);
-  requestData.send();
-}
-
-function handleResults() {
- 
-  var responseJSON = JSON.parse(this.response);
-  
-  if (responseJSON.error) console.log('Character not found');
-  else {
-    var html = '';
-     {
-      html += '<img src="'+ imageURL +'">';
-
-    }
-   
-   
-  }
-}
 
 
 
