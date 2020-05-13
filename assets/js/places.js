697bb06a5779b09bf273450e7ef602c2a6874eb1
Kasia Bee <kas.bogucka@gmail.com>
2020-05-13 23:38:51 +0000

    Some solutions found with regards to looping the images, needs to be tested

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,7 +1,7 @@
 
 
-
-$.getJSON('https://api.unsplash.com/search/photos?query=europe&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
+ $("button#asia").click(function(){
+$.getJSON('https://api.unsplash.com/search/photos?query=asia&per_page=30&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
 console.log(data);
 
 var imageList = data.results;
@@ -16,43 +16,32 @@ $.each(imageList, function(i, val){
 
     if(imageWidth>imageHeight){
         $('.output').append('<div class="image"><img src="'+ imageURL +'"></div>');
-        
 
 };
   });
 
   })
 
+})
+//   $("button#asia").click(function(){
 
+// $.getJSON('https://api.unsplash.com/search/photos?query=asia&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
+// console.log(data);
 
+// var imageList = data.results;
 
+// $.each(imageList, function(i, val){
 
-  $("button#asia").click(function(){
+//     var image = data.results[Math.floor(Math.random())]
 
-$.getJSON('https://api.unsplash.com/search/photos?query=asia&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
-console.log(data);
+//     var imageURL = val.urls.regular;
+//     var imageWidth = val.width;
+//     var imageHeight = val.height;
 
-var imageList = data.results;
+//     if(imageWidth>imageHeight){
+//         $('.output').append('<div class="image"><img src="'+ imageURL +'"></div>');
+//     }else{$('.output').append('<div class="image"><img src="'+ imageURL +'"></div>')}
+// });
+//   });
 
-$.each(imageList, function(i, val){
-
-    var image = data.results[Math.floor(Math.random())]
-
-    var imageURL = val.urls.regular;
-    var imageWidth = val.width;
-    var imageHeight = val.height;
-
-    if(imageWidth>imageHeight){
-        $('.output').append('<div class="image"><img src="'+ imageURL +'"></div>');
-    }else{$('.output').append('<div class="image"><img src="'+ imageURL +'"></div>')}
-});
-  });
-
-  })
-
-
-
-
-
-
-
+//   })
