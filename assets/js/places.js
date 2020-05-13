2e512741026171971c9a32f6121943ee768b85e7
Kasia Bee <kas.bogucka@gmail.com>
2020-05-13 00:03:30 +0000

    Looking for ways to connect api to searchbar

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,60 +1,71 @@
-// var url;
 
-// var button = document.getElementById('btn1');
-// button.addEventListener('click',startSearch);
-
-// function startSearch(event){
-//     var userInputValue = document.getElementById('searchBar').value;
-//     var urlBase = 'https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0/search/'
-//     if (userInputValue === null || userInputValue ==='') return;
-//     var searchUrl = urlBase + userInputValue
-//     performSearch(searchUrl);
-// }
-
-// function performSearch(searchUrl){
-//     var requestData = new XMLHttpRequest();
-//     requestData.addEventListener('load', handleResults);
-//     requestData.open('GET',searchUrl);
-//     requestData.send();
-// }
-
-// function handleResults(){
-//     var responseJSON = JSON.parse(this.response);
-
-//     if(responseJSON.error) console.log("Image not found");
-//     else {
-//         var html = '';
-//         responseJSON.results.forEach(function(result){
-//             html+='<div class="div.image-api"><img src="'+ result.image +'"></div>'
-//         })
-//         document.getElementById(".image-api");
-//     }
-// }
 
 $.getJSON('https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0', function(data) {
   console.log(data);
   let imageList = data.results;
- 
-
-  $.each(data, function(index,value){
-      console.log(value);
+  
+  $.each(data, function(i,value){
+      console.log(i, value);
   });
   
-  $.each(imageList, function(i, val) {
-    
-    
+  $.each(imageList, function(i, val) { 
     let imageURL = val.urls.regular;
-    let imageWidth = val.width;
-    let imageHeight = val.height;
-   
-    
-    if (imageWidth > imageHeight) {
-      $('header.output').append('<div class="div.image-api"><img src="'+ imageURL +'"></div>');
-    }   
-    
+    return  $('ul.output').append('<img src="'+ imageURL +'">') 
   });  
 
+const searchVal = document.getElementById('searchBar').value;
+$.each(imageList, function(i, val) { 
+    let imageURL = val.urls.regular;
+    return  $('section.output').append('<img src="'+ imageURL +'">') 
+  });
 
+
+
+ 
 });
+
+var button = document.getElementById('btn');
+// when user clicks on button, we want to call function start search
+button.addEventListener('click', startSearch);
+
+function startSearch(event) {
+
+  var userInputValue = document.getElementById('searchBar').value;
+ 
+  var urlBase = 'https://api.unsplash.com/search/photos?query=dublin&client_id=kZ9sSryGBssi-2T5OUPjVLye-DzTy5ksc6rEMov7fW0'
+ 
+  if (userInputValue === null || userInputValue === '') return;
+ 
+  var searchUrl = urlBase + userInputValue;
+  
+  performSearch(searchUrl);
+}
+
+function performSearch(searchUrl) {
+  
+  var requestData = new XMLHttpRequest();
+  
+  requestData.addEventListener('load', handleResults);
+  requestData.open('GET', searchUrl);
+  requestData.send();
+}
+
+function handleResults() {
+ 
+  var responseJSON = JSON.parse(this.response);
+  
+  if (responseJSON.error) console.log('Character not found');
+  else {
+    var html = '';
+     {
+      html += '<img src="'+ imageURL +'">';
+
+    }
+   
+   
+  }
+}
+
+
 
 
