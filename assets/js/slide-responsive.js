b044681e86e0184b5e18ab6eee8034a68b832431
Kasia Bee <kas.bogucka@gmail.com>
2020-05-16 20:23:03 +0000

    Added mock up Privacy Statement to the page

--- assets/js/slide-responsive.js
+++ assets/js/slide-responsive.js
@@ -1,36 +1,43 @@
-
 //Mobile media query for fading on scroll
 
 function myFunction(y) {
-  if (y.matches) { // If media query matches
+  if (y.matches) {
+    // If media query matches
     // $(document).ready(function(){
     //     $(window).scroll(function(){
     //         $("#slide").css("opacity", 1.4 - $(window).scrollTop() / ($('#slide').height() / 2));
     //     });
     // });
 
-
-
-$(document).ready(function(){
-        $(window).scroll(function(){
-            $("#slide4").css("opacity", 2.5 - $(window).scrollTop() / ($('#slide4').height() / 2));
-        });
+    $(document).ready(function () {
+      $(window).scroll(function () {
+        $("#slide4").css(
+          "opacity",
+          2.5 - $(window).scrollTop() / ($("#slide4").height() / 2)
+        );
+      });
     });
 
-$(document).ready(function(){
-        $(window).scroll(function(){
-            $("#slide5").css("opacity", 6.5 - $(window).scrollTop() / ($('#slide5').height() / 2));
-        });
+    $(document).ready(function () {
+      $(window).scroll(function () {
+        $("#slide5").css(
+          "opacity",
+          6.5 - $(window).scrollTop() / ($("#slide5").height() / 2)
+        );
+      });
     });
 
-$(document).ready(function(){
-        $(window).scroll(function(){
-            $("#slide6").css("opacity", 8 - $(window).scrollTop() / ($('#slide6').height()/ 2));
-        });
+    $(document).ready(function () {
+      $(window).scroll(function () {
+        $("#slide6").css(
+          "opacity",
+          8 - $(window).scrollTop() / ($("#slide6").height() / 2)
+        );
+      });
     });
+  }
 }
-}
 
-let y = window.matchMedia("(max-width: 500px)")
-myFunction(y) 
-y.addListener(myFunction)
+let y = window.matchMedia("(max-width: 500px)");
+myFunction(y);
+y.addListener(myFunction);
--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,18 +1,19 @@
 //Media query for fade on scroll - FHD and up (adapted from Stack Overflow and Media Queries on W3School - links in Readme)
 
 function myFunction(x) {
-  if (x.matches) { // If media query matches
-    $(document).ready(function(){
-        $(window).scroll(function(){
-            $("#slide").css("opacity", 3 - $(window).scrollTop() / ($('#slide').height() / 2));
-        });
+  if (x.matches) {
+    // If media query matches
+    $(document).ready(function () {
+      $(window).scroll(function () {
+        $("#slide").css(
+          "opacity",
+          2 - $(window).scrollTop() / ($("#slide").height() / 2)
+        );
+      });
     });
-
-
-  } 
+  }
 }
 
-let x = window.matchMedia("(min-width: 1200px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
-
+let x = window.matchMedia("(min-width: 1200px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
