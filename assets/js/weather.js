2ab01bd4bbec0bea3fc2e8bf26ae0a3f2fc37c4b
Kasia Bee <kas.bogucka@gmail.com>
2020-05-17 15:40:57 +0000

    Users position and error coded into the weather card. Checked that the error is displaying

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,90 +1,47 @@
 
-// //api.openweathermap.org/data/2.5/weather?lat=latitude&lon=longitude&APPID=5e27cf085bbe7c169960470c30456c13
+//api.openweathermap.org/data/2.5/weather?lat=latitude&lon=longitude&APPID=5e27cf085bbe7c169960470c30456c13
 
-// //App constants and variables
-// const KELVIN = 273;
-// //API key
-// const key = "5e27cf085bbe7c169960470c30456c13";
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".weather-icon");
+const notificationElement = document.querySelector(".notification");
 
-// function getWeather(latitude, longitude){
-//     let api=`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-//     fetch(api).then(function(response){
-//         let data = response.json();
-//         return data;
-//     })
-//     .then(function(data){
-//         weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-//         weather.description = data.weather[0].description;
-//         weather.iconId = data.weather[0].icon;
-//         weather.city = data.name;
-//         weater.country = data.sys.country;
-//     })
-//     .then
-// }
+//App data
 
+const weather = {};
 
+weather.temperature = {
+    unit: "celsius"
+}
 
-// // const weather = {
+//App constants and variables
+const KELVIN = 273;
+//API KEY
+const key = "5e27cf085bbe7c169960470c30456c13";
 
-// //     temperature:{
-// //         value:18,
-// //         unit: "celsius"
-// //     },
-// //     description: "few clouds",
-// //     iconId: "01d",
-// //     city: "London",
-// //     country: "GB"
-// // }
+//Check if browser supports geolocation
 
-// // /*---Reusable function-----*/
-// // displayWeather(){
-// // iconElement.innerHTML=
-// // `<img src="icons/${weather.iconId}.png"/>`;
+if("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display="block";
+    notificationElement.innerHTML = "<p>Browser doesn't support geolocation</p>";
+}
 
-// // iconElement.innerHTML=
-// // `${weather.temperature.value}°<span>C</span>`;
+//Set users position
+function setPosition(position){
+    let latitude=position.coords.latitude;
+    let longitude=position.coords.latitude;
 
-// // descElement.innerHTML=
-// // weather.description;
+    getWeather(latitude, longitude);
+}
 
-// // locationElement.innerHTML =
-// // `${weather.city},${weather.country}`;
-// // }
-// // /*------------Converting from Kalvins to Celsius-----*/
-// // weather.temperature.value = 300-273
-// // /*---------Converting from Celsius to Farenheit-------*/
-// // function celsiusToFarenheit( temperature  ){
-// // return (temperature *9/5) +32;
-// // }
+//show error when there is an issue with geolocation service
 
-// // tempElement.addEventListener("click", function(){
-// //     if(weather.temperature.unit === "celsius"){
-// //         celsiusToFarenheit(weather.temperature.value);
-// //         farenheit = Math.floor(farenheit);
-// //         tempElement.innerHTML = `${farenheit}°<span>F</span>`;
-// //         weather.temperature.unit = "farenheit";
-// //     } else {
-// //         tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-// //         weather.temperature.unit = "celsius";
-// //     }
-// // })
+function showError(error){
+    notificationElement.style.display="block";
+    notificationElement.innerHTML = `<p>${error.message}</p>`;
+}
 
-// // getCurrentPosition(setPosition, error );
-
-// // function setPosition(position){
-// // let latitude = position.coords.latitude;
-// // let latitude = position.coords.longitude;
-// // getWeather(latitude, longitude);
-// // }
-
-// // function showError(error){
-// // notificationElement.style.display="block";
-// // notificationElement.innerHTML=`<p> ${error.message}</p>`;
-// // }
-
-// // if("geolocation" in navigator){
-// //     navigator.geolocation.getCurrentPosition ( setPosition, showError );
-// // }else{
-// //     notificationElement.style.display = "block";
-// //     notificationElement.innerHTML = "<p>Browser Doesn't Support Geolocation.</p>"
-// // }
+//Get weather from API provider
