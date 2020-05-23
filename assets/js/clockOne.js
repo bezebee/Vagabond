d7c081b30f93e8d6c85a6701a6ef4d6080f5acd3
Kasia Bee <kas.bogucka@gmail.com>
2020-05-23 13:46:49 +0000

    Clocks built and formatted for responsiveness. Working on applying the time-zone function

--- assets/js/clockOne.js
+++ assets/js/clockOne.js
@@ -1,3 +1,5 @@
+
+
 setInterval(setClock, 1000)
 
 const hourHand = document.querySelector('[data-hour-hand]')
@@ -5,7 +7,7 @@ const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 
 function setClock(){
-    const currentDate = new Date()
+    const currentDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' })
     const secondsRatio = currentDate.getSeconds() /60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) /60
     const hoursRatio = (minutesRatio + currentDate.getHours()) /12
